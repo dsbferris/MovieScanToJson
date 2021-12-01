74d92a5fd90ade978c8456837226f483b6762665
Ferris Schwanebeck <ferris.s@aol.de>
2021-12-01 14:58:20 +0100

    Projektdateien hinzufügen.

---
+++ MovieScanToJson/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<List<string>> list_of_list_of_folders = new();
+list_of_list_of_folders.Add(new () {
+    @"\\location1", @"\\location2", @"\\location3", @"\\location4", @"\\location5" });
+list_of_list_of_folders.Add(new () {
+    @"\\movies1", @"\\movies2", @"\\movies3", @"\\movies4", @"\\movies5"});
+
+foreach(var list_of_folders in list_of_list_of_folders)
+{
+
+}
