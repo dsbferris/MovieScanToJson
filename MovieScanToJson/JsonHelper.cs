ffc275474f5ac4b58406fdf6dd5bffb5186a5547
Ferris Schwanebeck <ferris.s@aol.de>
2021-12-07 10:37:25 +0100

    Finished Encryption for Json. Finished Project.

--- MovieScanToJson/JsonHelper.cs
+++ MovieScanToJson/JsonHelper.cs
@@ -1,32 +1,78 @@
-﻿using System.Text.Json;
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System;
 
 namespace MovieScanToJson
 {
     public static class JsonHelper
     {
         public const string jsonfile = "movies.json";
-        private static readonly JsonSerializerOptions options = new() { WriteIndented = true };
+        public const string cryptojsonfile = "movies.json.encrypted";
 
-        public static async Task WriteToFileAsync<T>(T data, string filepath = jsonfile)
+        private const string KeyString = "WatchingMovies12"; //256-bit Key
+        private static readonly byte[] Key = Encoding.Unicode.GetBytes(KeyString);
+
+        private static readonly JsonSerializerOptions jsonOptions = new() { WriteIndented = true };
+        private static readonly FileStreamOptions writeOptions = new()
+        {
+            Mode = FileMode.CreateNew,
+            Access = FileAccess.Write,
+            Options = FileOptions.Asynchronous
+        };
+        private static readonly FileStreamOptions readOptions = new()
+        {
+            Access = FileAccess.Read,
+            Mode = FileMode.Open,
+            Options = FileOptions.Asynchronous
+        };
+
+        public static async Task SerializeToFileAsync<T>(T data, string filepath = jsonfile)
         {
             using FileStream fs = File.Create(filepath);
-            await JsonSerializer.SerializeAsync(fs, data, options);
+            await JsonSerializer.SerializeAsync(fs, data, jsonOptions);
         }
 
-        public static async Task<T?> ReadFromFileAsync<T>(string filepath = jsonfile)
+        public static async Task<T?> DeserializeFromFileAsync<T>(string filepath = jsonfile)
         {
             using FileStream fs = File.OpenRead(filepath);
-            return await JsonSerializer.DeserializeAsync<T>(fs, options);
+            return await JsonSerializer.DeserializeAsync<T>(fs, jsonOptions);
         }
 
-        public static async Task WriteToStream<T>(T data, Stream stream)
+        public static async Task SerializeToEncryptedFile<T>(T data, string filepath = cryptojsonfile)
         {
-            await JsonSerializer.SerializeAsync(stream, data, options);
+            using var aes = Aes.Create();
+            aes.Key = Key;
+            await using FileStream fs = new(filepath, writeOptions);
+
+            //write IV to begin of file
+            await fs.WriteAsync(aes.IV.AsMemory(0, aes.IV.Length));
+
+            await using CryptoStream cs = new(fs, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            await JsonSerializer.SerializeAsync<T>(cs, data, jsonOptions);
         }
 
-        public static async Task<T?> ReadFromStreamAsync<T>(Stream stream)
+        public static async Task<T?> DeserializeFromEncryptedFile<T>(string filepath = cryptojsonfile)
         {
-            return await JsonSerializer.DeserializeAsync<T>(stream, options);
+            using var aes = Aes.Create();
+            aes.Key = Key;
+            await using FileStream fs = new(filepath, readOptions);
+
+            //read IV from begin of file
+            byte[] iv = new byte[aes.IV.Length];
+            int numBytesToRead = aes.IV.Length;
+            int numBytesAlreadyRead = 0;
+            while(numBytesToRead > 0)
+            {
+                int n = await fs.ReadAsync(iv.AsMemory(numBytesAlreadyRead, numBytesToRead));
+                if (n == 0) break;
+                numBytesAlreadyRead += n;
+                numBytesToRead -= n;
+            }
+            aes.IV = iv;
+
+            await using CryptoStream cs = new(fs, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            return await JsonSerializer.DeserializeAsync<T>(cs, jsonOptions);
         }
     }
 }
--- MovieScanToJson/Program.cs
+++ MovieScanToJson/Program.cs
@@ -5,35 +5,64 @@ using System.Diagnostics;
 string[] hds = { @"\\192.168.178.39\hd1", @"\\192.168.178.39\hd2", @"\\192.168.178.39\hd3", @"\\192.168.178.39\hd4", @"\\192.168.178.39\hd5" };
 // string[] mvs = { @"\\192.168.178.39\movies1", @"\\192.168.178.39\movies2", @"\\192.168.178.39\movies3", @"\\192.168.178.39\movies4", @"\\192.168.178.39\movies5" };
 
-
-#region Explorer NAS Login
-
-if (!Directory.Exists(hds[0]))
+void checkNas()
 {
-    Console.WriteLine("Remember to first login into NAS before accessing it!");
-    Console.WriteLine("Do you want to open explorer to login? (y/n)");
-    var yesno = Console.ReadLine();
-    if (yesno != null)
+    if (!Directory.Exists(hds[0]))
     {
-        if (yesno.ToLower() == "yes" || yesno.ToLower() == "y")
+        Console.WriteLine("Remember to first login into NAS before accessing it!");
+        Console.WriteLine("Do you want to open explorer to login? (y/n)");
+        var yesno = Console.ReadLine();
+        if (yesno != null)
         {
-            ProcessStartInfo psi = new();
-            psi.FileName = @"explorer.exe";
-            psi.Arguments = @"\\192.168.178.39\";
-            Process p = new() { StartInfo = psi };
-            p.Start();
-            Console.WriteLine("Press enter to continue after login.");
-            Console.ReadLine();
+            if (yesno.ToLower() == "yes" || yesno.ToLower() == "y")
+            {
+                ProcessStartInfo psi = new();
+                psi.FileName = @"explorer.exe";
+                psi.Arguments = @"\\192.168.178.39\";
+                Process p = new() { StartInfo = psi };
+                p.Start();
+                Console.WriteLine("Press enter to continue after login.");
+                Console.ReadLine();
+            }
         }
     }
 }
 
-#endregion
+async Task readAndEncryptFileAndExit()
+{
+    var json_movies = await JsonHelper.DeserializeFromFileAsync<List<MovieModel>>();
+    if (json_movies == null)
+    {
+        Console.WriteLine("json_movies is null...");
+        return;
+    }
+    else
+    {
+        await JsonHelper.SerializeToEncryptedFile<List<MovieModel>>(json_movies);
+        var crypto_movies = await JsonHelper.DeserializeFromEncryptedFile<List<MovieModel>>();
+        if (crypto_movies == null)
+        {
+            Console.WriteLine("crypto_movies is null...");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("About to exit");
+            return;
+        }
+    }
+}
 
+async Task readMoviesAndSaveToEncryptedJson()
+{
+    var movies = MovieGlobberAsync.GetAllMoviesSorted(hds);
 
-var movies = MovieGlobberAsync.GetAllMoviesSorted(hds);
+    Console.WriteLine("Found " + movies.Count + " movies.");
+    Console.WriteLine("Writing found movies to json file");
+    await JsonHelper.SerializeToEncryptedFile<List<MovieModel>>(movies);
+    Console.WriteLine($"Written encrypted json to {JsonHelper.cryptojsonfile}");
+}
 
-Console.WriteLine("Found " + movies.Count + " movies.");
-Console.WriteLine("Writing found movies to json file");
-await JsonHelper.WriteToFileAsync<List<MovieModel>>(movies);
-Console.WriteLine($"Written json to {JsonHelper.jsonfile}");
+// await readAndEncryptFileAndExit();
+checkNas();
+await readMoviesAndSaveToEncryptedJson();
